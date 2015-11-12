c03f9b8cbfa61a0c050a95cad96cfd4901bc3085
suroor <surusuroor@gmail.com>
2015-11-12 16:28:35 +0530

    It works!!
    *made a crude implementation

---
+++ primetest.py
@@ -0,0 +1,30 @@
+from sys import argv
+
+script, limit = argv
+
+def primes(n) :
+    num = range(2, n+1)
+    primes = []
+    # for i in range(2,n+1) :
+    #     num.append(i)
+    print num
+    while len(num) != 0  :
+        temp = num[0]
+        primes.append(temp)
+        sieve = [temp]
+        j = 0
+        while n > sieve[-1] :
+            # print "before", j
+            sieve.append(temp * num[j])
+            # print sieve
+            # print sieve[-1]
+            j += 1
+            # print "after", j
+            # print "sieve", sieve
+        temp_list = [x for x in num if x not in sieve]
+        print "temp list", temp_list
+        num = temp_list
+        print "prime", primes
+        print "num", num
+
+primes(int(limit))
